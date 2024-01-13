61bd7502f24cfab262dfd5b39f84d0ca31af7a4b
Paulo Henrique <contato@paulohenriquesn.com>
2024-01-13 16:45:44 -0300

    refactor: sql connection

--- src/infra/db/pg.py
+++ src/infra/db/pg.py
@@ -1,16 +1,9 @@
 from dotenv import load_dotenv
 import os
-import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 
 load_dotenv()
-
-connection = psycopg2.connect(database=os.environ["DB"],
-                              host=os.environ["DB_HOST"],
-                              user=os.environ["DB_USER"],
-                              password=os.environ["DB_PASSWORD"],
-                              port=os.environ["DB_PORT"])
 
 url = URL.create(
     drivername="postgresql",
