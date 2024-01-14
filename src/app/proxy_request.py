c6ffb3002623739ccbc760edc867fbff79617db8
Paulo Henrique <contato@paulohenriquesn.com>
2024-01-14 15:33:19 -0300

    feat: add log on body proxy

--- src/app/proxy_request.py
+++ src/app/proxy_request.py
@@ -1,8 +1,10 @@
 import json
 
+
 def proxy_request(controller, event):
     event_json = json.loads(json.dumps(event))
     body = None
     if event_json['body']:
         body = json.loads(event_json['body'])
+        print(body)
     return controller.handle(body)
