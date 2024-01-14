a37ab7023ccc4ecd0b4d29bbcfbe529980e5edde
Paulo Henrique <contato@paulohenriquesn.com>
2024-01-14 15:10:56 -0300

    feat: add delete abstract method on repository

--- src/domain/repositories/task.py
+++ src/domain/repositories/task.py
@@ -4,9 +4,13 @@ from abc import ABC, abstractmethod
 
 class TaskRepository(ABC):
     @abstractmethod
-    def create(self, task: Task):
+    def create(self, task: Task) -> None:
         pass
 
     @abstractmethod
     def list(self) -> list[Task]:
+        pass
+
+    @abstractmethod
+    def delete(self, id: str) -> None:
         pass
